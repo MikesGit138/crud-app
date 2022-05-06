049463e6abc161ba9417c6cd03772b9a03dc1b07
Michael <michael138leighton@gmail.com>
2022-05-06 07:32:49 -0500

    i

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const ejs = require('ejs')
 const mysql = require('mysql')
-const { userInfo } = require('os')
-const { constants } = require('buffer')
+// const { userInfo } = require('os')
+// const { constants } = require('buffer')
 const app = express()
 
 // app.get('/', (req, res) => {
@@ -52,19 +52,43 @@ app.get('/add', (req,res) =>{
     })
 })
 
-// app.post('/save', (req,res) => {
-//     let data = {
-//         pro_title: req.body.projectTitle,
-//         pro_description: req.body.description,
-//         pro_start: req.body.start,
-//         pro_due: req.body.due
-//     }
-//     let sql = "INSERT INTO projects SET ?"
-//     let query = connection.query(sql, data, (err, results) =>{
-//         if(err) throw err
-//         res.redirect('/')
-//     })
-// })
+app.post('/save', (req,res) => {
+    let data = {
+        project_title: req.body.projectTitle,
+        project_description: req.body.description,
+        project_start_dt: req.body.start,
+        project_due_dt: req.body.due
+    }
+    let sql = "INSERT INTO userinfo.projects SET ?"
+    let query = connection.query(sql, data, (err, results) =>{
+        if(err) throw err
+        res.redirect('/')
+    })
+})
+
+app.get('/edit/:userId',(req,res)=> {
+    const userId = req.params.userId
+    let sql = `SELECT * FROM userinfo.projects WHERE id = ${userId}`
+    let query = connection.query(sql, (err, result) =>{
+        if (err) throw err
+        res.render('user_edit', {
+            title: 'Edit page',
+            projects: result[0]
+
+        })
+    })
+})
+
+app.post('/update',(req, res) => {
+    const userId = req.body.id;
+    let sql = "update userinfo.projects SET title='"+req.body.project_title+"', description='"+req.body.project_description+"', start='"+req.body.project_start_dt+"' where id ="+userId;
+    let query = connection.query(sql,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+ 
+
 
 //server listening
 app.listen (3000, () => {
